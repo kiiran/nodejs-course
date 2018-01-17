680ac70fef83225bc186cf737b4fb3557446f2df
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-17 11:47:37 +0000

    Added oauth

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// auth
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleID,
+      clientSecret: keys.googleSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken) => {
+      console.log('accessToken', accessToken);
+    }
+  )
+);
+
 // routes
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 app.listen(PORT);

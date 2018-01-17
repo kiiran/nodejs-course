41f78da52abcac2b2bd09cf43dd563b937296a22
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-17 13:52:24 +0000

    Refactored the index file and restructured the routes

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
 const app = express();
+
+// This is the same as requiring the exported function from authRoutes
+// as a named const and then calling the function like authRoutes(app)
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
-
-// auth
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleID,
-      clientSecret: keys.googleSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
-
-// routes
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleID,
+      clientSecret: keys.googleSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);

6b4ca3cef3a92500f0403f2f6758d8de4ef72173
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-17 15:27:44 +0000

    Added User model and sign-in process

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+// Required for using cookies
+app.use(passport.initialize());
+app.use(passport.session());
 
 // This is the same as requiring the exported function from authRoutes
 // as a named const and then calling the function like authRoutes(app)

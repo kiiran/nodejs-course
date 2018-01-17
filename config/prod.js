6b03cb63677efbfa281938adac3237aa505a4d61
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-17 16:17:28 +0000

    Set up keys file to toggle between dev and prod keys

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// production keys
+module.exports = {
+  googleID: process.env.GOOGLE_CLIENT_ID,
+  googleSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};

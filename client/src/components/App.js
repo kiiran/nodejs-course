921a0ac53c81fd2fdf95657e55036306779abf27
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-18 11:04:48 +0000

    Added a navbar and some styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 
@@ -9,12 +9,12 @@ const Landing = () => <h2>Landing</h2>;
 const App = () => {
   return (
     <BrowserRouter>
-      <Fragment>
+      <div className="container">
         <Header />
         <Route exact path="/" component={Landing} />
         <Route exact path="/surveys" component={Dashboard} />
         <Route path="/surveys/new" component={SurveyNew} />
-      </Fragment>
+      </div>
     </BrowserRouter>
   );
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
-    return <header>Header</header>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo">
+            Emaily
+          </Link>
+          <ul id="nav-mobile" className="right">
+            <li>
+              <Link to="/header">Log in</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 

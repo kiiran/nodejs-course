f14b28d83f9c3385263179717430ee5629465c06
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-18 17:15:07 +0000

    Created Recipient to be a child of Survey and added link to User

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
+const recipientSchema = require('./Recipient');
+
 const { Schema } = mongoose;
 
 const surveySchema = new Schema({
   title: String,
   subject: String,
   body: String,
-  recipients: [String]
+  recipients: [recipientSchema],
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 },
+  dateSent: Date,
+  lastResponded: Date,
+  // the underscore here is convention: it's to indicate
+  // that it's a relationship to another model
+  _user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 mongoose.model('surveys', surveySchema);

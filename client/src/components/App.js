ae406c1fcef62f4e60ad6313d16aa22bedf20076
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-18 10:41:21 +0000

    Added react-dom-router and some placeholder components

---
+++ client/src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Fragment>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route path="/surveys/new" component={SurveyNew} />
+      </Fragment>
+    </BrowserRouter>
+  );
+};
+
+export default App;

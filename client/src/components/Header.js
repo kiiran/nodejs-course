e052cda9d9bc58b8ef14cbeec642e81fecb53196
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-18 12:53:33 +0000

    Log in/out

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderLinks() {
+    switch (this.props.auth) {
+      case null:
+        return <li>Spinner...</li>;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Log in</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Log out</a>
+          </li>
+        );
+    }
+  }
+
   render() {
+    console.log('header props', this.props);
     return (
       <nav>
         <div className="nav-wrapper">
@@ -10,9 +31,7 @@ class Header extends Component {
             Emaily
           </Link>
           <ul id="nav-mobile" className="right">
-            <li>
-              <Link to="/header">Log in</Link>
-            </li>
+            {this.renderLinks()}
           </ul>
         </div>
       </nav>
@@ -20,4 +39,6 @@ class Header extends Component {
   }
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => ({ auth });
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -1,6 +1,10 @@
-export default function authReducers(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+
+export default function authReducers(state = null, action) {
   console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }

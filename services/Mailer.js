a39d9f6e0f71443a5a7f58a3b75007aa8edfcc9d
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-19 10:09:00 +0000

    Added Mailer send function

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email('no-reply@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -37,6 +38,17 @@ class Mailer extends helper.Mail {
 
   formatAddresses(recipients) {
     return recipients.map(({ email }) => new helper.Email(email));
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = await this.sgApi.API(request);
+    return response;
   }
 }
 

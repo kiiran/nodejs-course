31ae3ce16c59f282ee6d14318997eda5da2bb99c
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-19 09:39:41 +0000

    Add code to the Mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,7 +3,41 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
-  // Mailer code goes here
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+
+    // this function comes from helper.Mail
+    this.addContent(this.body);
+
+    // these ones don't
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach(recipient => personalize.addTo(recipient));
+
+    this.addPersonalization(personalize);
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => new helper.Email(email));
+  }
 }
 
 export default Mailer;

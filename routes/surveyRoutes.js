ec0da4257971554cc7ed6ae9ac48bb8d9bbfa4dc
Ciaran Edwards <ciaran.edwards@live.com>
2018-01-19 10:58:19 +0000

    Send email, save survey, deduct credit from the user, and send response

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -21,9 +21,17 @@ module.exports = app => {
 
     // send the emails
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
 
-    // survey.save();
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {});
